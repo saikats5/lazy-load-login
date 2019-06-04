f080f707b2d44c7485584af162031ec58c035fff
Saikat <saikats_5@yahoo.co.in>
2019-06-04 21:21:23 +0530

    auth service

--- lazyloadinglogin/src/app/guards/auth.guard.ts
+++ lazyloadinglogin/src/app/guards/auth.guard.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+  constructor(public authService: AuthService, public router: Router){
+
+  }
+  canActivate(): boolean {
+    if(!this.authService.isAuthenticated()){
+      this.router.navigate(['login']);
+      return false;
+    }
     return true;
   }
 }
